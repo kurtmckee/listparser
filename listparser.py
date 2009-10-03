e29978f14af05cfd2f544e39db4533f66eec3513
Kurt McKee <contactme@kurtmckee.org>
2009-10-03 13:06:03 -0500

    version 0.9

--- listparser.py
+++ listparser.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.8"
+__version__ = "0.9"
 
 import copy
 import datetime
