7a0be553a1eb297b4f1b138ecde9a4e0e9625872
Kurt McKee <contactme@kurtmckee.org>
2009-12-25 08:25:13 -0600

    version 0.11

--- setup.py
+++ setup.py
@@ -6,12 +6,7 @@ setup(
     name='listparser',
     version=__version__,
     description='Parse subscription lists into a consistent format.',
-    long_description="""
-        listparser is a Python library that parses subscription lists (also
-        called reading lists) and returns all of the feeds and subscription
-        lists that it finds. It currently supports OPML and the iGoogle
-        exported settings format.
-    """,
+    long_description="""listparser is a Python library that parses subscription lists (also called reading lists) and returns all of the feeds, subscription lists, and "opportunity" URLs that it finds. It supports OPML, RDF+FOAF, and the iGoogle exported settings format.""",
     author='Kurt McKee',
     author_email='contactme@kurtmckee.org',
     url='http://freshmeat.net/projects/listparser',
