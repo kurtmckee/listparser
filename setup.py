06281b5f48e8d3ad7cf92ab1ff11932286b2e297
Kurt McKee <contactme@kurtmckee.org>
2009-12-27 13:30:28 -0600

    Add `classifiers` to the setup.py script

--- setup.py
+++ setup.py
@@ -11,4 +11,19 @@ setup(
     author_email='contactme@kurtmckee.org',
     url='http://freshmeat.net/projects/listparser',
     py_modules=['listparser'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Java',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Text Processing :: Markup :: XML',
+    ]
 )
