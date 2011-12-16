16fb9f4c14ed4c50c0e64972695483636b906126
Kurt McKee <contactme@kurtmckee.org>
2011-12-16 02:44:52 -0600

    Auto-convert listparser using 2to3 in Python 3

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+import sys
 
-from listparser import __version__
+extra = {}
+if sys.version_info >= (3, ):
+    extra['use_2to3'] = True
 
 setup(
     name='listparser',
-    version=__version__,
+    version='0.15',
     description='Parse subscription lists into a consistent format.',
     long_description="""listparser is a Python library that parses subscription lists (also called reading lists) and returns all of the feeds, subscription lists, and "opportunity" URLs that it finds. It supports OPML, RDF+FOAF, and the iGoogle exported settings format.""",
     author='Kurt McKee',
@@ -21,9 +24,13 @@ setup(
         'Programming Language :: Python :: 2.4',
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
         'Topic :: Software Development :: Libraries',
         'Topic :: Text Processing :: Markup :: XML',
-    ]
+    ],
+    **extra
 )
