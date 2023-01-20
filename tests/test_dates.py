f28e7187d1a6e56ff928ce523a98ffe12da19077
Kurt McKee <contactme@kurtmckee.org>
2023-01-20 13:53:44 -0600

    Update the copyright years

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -1,5 +1,5 @@
 # This file is part of listparser.
-# Copyright 2009-2022 Kurt McKee <contactme@kurtmckee.org>
+# Copyright 2009-2023 Kurt McKee <contactme@kurtmckee.org>
 # SPDX-License-Identifier: MIT
 #
 
