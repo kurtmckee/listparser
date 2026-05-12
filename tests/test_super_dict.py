a24b3267f8158c0b2ef3c64d24da5948b9c9cfff
Kurt McKee <contactme@kurtmckee.org>
2026-05-12 17:32:35 -0500

    Run `tox run -m update`

--- tests/test_super_dict.py
+++ tests/test_super_dict.py
@@ -1,5 +1,5 @@
 # This file is part of listparser.
-# Copyright 2009-2025 Kurt McKee <contactme@kurtmckee.org>
+# Copyright 2009-2026 Kurt McKee <contactme@kurtmckee.org>
 # SPDX-License-Identifier: MIT
 
 import pytest
